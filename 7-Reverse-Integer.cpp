11959bafe932f0f4c7b64d9944366728d4191a80
cindylaii <hylaii@gmail.com>
2019-06-08 16:41:49 +0800

    Update 7-Reverse-Integer.cpp
    remove debug message

--- 7-Reverse-Integer.cpp
+++ 7-Reverse-Integer.cpp
@@ -21,30 +21,18 @@ public:
         while(x>0)  //For example 123
         {
             int y= x%10;   //y=3
-            cout << "x%10: "<< x%10 << endl;
             numberArray.push_back(y);
             x=x/10;        //x=12     
         }
-        printf("\n");
-        
-        cout << "result: ";
-        for(int i=0; i<numberArray.size(); ++i)
-        {
-            cout << numberArray.at(i) << " ";
-        }
-        cout <<endl;
-        
-        printf("pow(10, 0)=%d\n", pow(10, 0));
         
         //numberArray is 3 2 1
         long long result=0;
         result= result + numberArray.back();
         int power=1;
-        cout << "result= "<< result;
+
         for(int i=numberArray.size()-2; i>=0; i--)
         {
             result = result+ numberArray[i]*pow(10, power);
-            cout << "result= "<< result;
             power++;
         }
 
@@ -54,8 +42,6 @@ public:
         if(result < (-pow(2,31)))
             return 0;
 
-        
-        cout << "answer is " << result <<endl;
 
         return (isMinus)?(-result):result;
         
