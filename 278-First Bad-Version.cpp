ecf212d84120626655bd8a1707d3f8679908b009
cindylaii <hylaii@gmail.com>
2019-07-20 22:23:09 +0800

    Create 278-First Bad-Version.cpp

---
+++ 278-First Bad-Version.cpp
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/first-bad-version/
+//278. First Bad Version
+//278-First Bad-Version.cpp
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+
+#if 0
+
+Scenario #1: isBadVersion(mid) => false
+
+ 1 2 3 4 5 6 7 8 9
+ G G G G G G B B B       G = Good, B = Bad
+ |       |       |
+left    mid    right
+    
+ Scenario #2: isBadVersion(mid) => true
+
+ 1 2 3 4 5 6 7 8 9
+ G G G B B B B B B       G = Good, B = Bad
+ |       |       |
+left    mid    right   
+
+#endif
